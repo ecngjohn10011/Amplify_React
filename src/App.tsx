489b65f3b1ef3a8357c6b39039b29c50672e4574
ecngjohn <48666535+ecngjohn@users.noreply.github.com>
2022-11-30 17:51:52 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+
+      <h1 className="text-5xl font-bold">
+           ToDo APp
+      </h1>
+
+      {/* Form */}
+      
+
+
+
+    </div>
+  )
+}
+
+export default App
